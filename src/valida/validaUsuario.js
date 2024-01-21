e44d5f6c1c8d8e7a74954b4ceb91d18b6d9e6978
AnaPaulaDosSantos <102711383+AnaPaulaDosSantos@users.noreply.github.com>
2024-01-21 17:27:33 -0300

    Alterações

---
+++ src/valida/validaUsuario.js
@@ -0,0 +1,25 @@
+const joi = require("joi");
+
+const schemaUsuario = joi.object({
+    nome: joi.string().required().messages({
+        "any.required": "O campo nome é obrigatório",
+
+        "string.empty": "O campo nome é obrigatório",
+    }),
+    email: joi.string().email().required().messages({
+
+        "string.email": "O campo email precisa ter um formato válido",
+
+        "any.required": "O campo email é obrigatório",
+
+        "string.empty": "O campo email é obrigatório",
+    }),
+    senha: joi.string().required().messages({
+
+        "any.required": "O campo senha é obrigatório",
+
+        "string.empty": "O campo senha é obrigatório",
+    }),
+});
+
+module.exports = valdiaUsuario;
