bb6f3f6f2118d6b5c4dc49545591a69e6042b9f7
thauanb11 <guedesbruno40@gmail.com>
2024-02-04 10:45:58 -0300

    validação e aprimoramento de exclusao de produtos

--- src/controladores/privado/produtos/excluirProduto.js
+++ src/controladores/privado/produtos/excluirProduto.js
@@ -2,13 +2,31 @@ const knex = require('../../../config/conexaoDB');
 const joi = require("joi");
 
 const excluirProduto = async (req, res) => {
-    const id = req.params.id;
     try {
+        const { id } = req.body
 
-        //realiza a exclusão do produto no banco de dados
-        const produto = await knex('produtos').where({id}).del();
+        const schema = joi.object({
+            id: joi.number().require()
+        });
 
-        return res.status(200).json({ message: 'Produto excluído com sucesso' });
+        const validação = schema.validate({ id });
+
+        if (validação.error) {
+            return res.status(400).json({ erro: validação.error.details[0].message });
+        }
+
+        //realiza a verificação do produto antes da exclusão
+        const produtoExistente = await knex('produtos').where({ id }).first();
+
+        if (!produtoExistente) {
+            return res.status(400).json({ erro: 'Produto não encontrado!' })
+        }
+
+        //realiza a exclusão do produto no bd
+        await knex('produtos').where({ id }).del();
+
+        return res.status(200).json({ messagem: 'Produto excluido com sucesso!' })
+
     } catch (error) {
         return res.status(500).json({ error: 'Erro ao excluir o produto' });
     }
