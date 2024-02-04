04ee7abb56bf2a4d02b7be0e45c3df82ad8bfa04
Deyweson <devdeyve@gmail.com>
2024-02-04 13:25:31 -0300

    ajustes

--- src/controladores/privado/produtos/cadastroProduto.js
+++ src/controladores/privado/produtos/cadastroProduto.js
@@ -12,23 +12,27 @@ const cadastrarProduto = async (req, res) => {
         if (!categoriaExiste) {
             return res.status(404).json({ mensagem: 'A categoria informada não existe.' });
         }
-
+        
         // Upload da imagem
-        const uploadImagem = await s3.upload({
-            Bucket: process.env.BB_KEY_NAME,
-            Key: descricao,
-            Body: file.buffer,
-            ContentType: file.mimetype
-        }).promise()
-        console.log(uploadImagem)
+        if(file){
+            const uploadImagem = await s3.upload({
+                Bucket: process.env.BB_KEY_NAME,
+                Key: descricao,
+                Body: file.buffer,
+                ContentType: file.mimetype
+            }).promise()
 
-        const produto_imagem = uploadImagem.Location
+            console.log(uploadImagem)
+            const produto_imagem = uploadImagem.Location
+            const novoProduto = await knex('produtos').insert({ descricao, quantidade_estoque, valor, categoria_id, produto_imagem: produto_imagem }).returning('*');
+            
+            return res.status(201).json(novoProduto);
+        }
 
         // Inserir o novo produto
-        const novoProduto = await knex('produtos').insert({ descricao, quantidade_estoque, valor, categoria_id, produto_imagem: produto_imagem }).returning('*');
-
+        const novoProduto = await knex('produtos').insert({ descricao, quantidade_estoque, valor, categoria_id,}).returning('*');
         return res.status(201).json(novoProduto);
-
+        
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ mensagem: 'Erro interno do servidor' });
